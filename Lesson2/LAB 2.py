ff596496cc47a6b29937be87ec2637f048238a66
ronripen <Troncho20032>
2022-03-28 16:16:11 +0300

    lab2

---
+++ Lesson2/LAB 2.py
@@ -0,0 +1,3 @@
+prices = {'Tomato': 3, 'Cucamber': 2, 'coke': 5, 'Chicken': 20}
+total = int([sum((int(input(f'how many {i}s would you like to buy? ')) * prices[i]) for i in prices.keys())][0])
+print(f'your total is {total}')
