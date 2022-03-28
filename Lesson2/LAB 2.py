0db9c1a76451ca378941d8eec129cdd6087780b7
ronripen <Troncho20032>
2022-03-28 16:26:12 +0300

    lab2

--- Lesson2/LAB 2.py
+++ Lesson2/LAB 2.py
@@ -1,4 +1,4 @@
 prices = {'Tomato': 3, 'Cucamber': 2, 'coke': 5, 'Chicken': 20}
 total = int([sum((int(input(f'how many {i}s would you like to buy? ')) * prices[i]) for i in prices.keys())][0])
 print(f'your total is {total} before tax')
-print(f'your total is {str("%.2f"%(total * 1.17))} taxed')
+print(f'your total is {"%.2f"%(total * 1.17)} taxed')
