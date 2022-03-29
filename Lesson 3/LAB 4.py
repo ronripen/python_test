7762566deb75dd2fdd37bf7786b687c240a61c98
ronripen <Troncho20032>
2022-03-29 13:10:50 +0300

    lab4

--- Lesson 3/LAB 4.py
+++ Lesson 3/LAB 4.py
@@ -1,4 +1,9 @@
 filename = "C:/Users/USER/Desktop/hermelin/python/files/test.txt"
-f = open(filename, 'r')
+#f = open(filename, 'r')
+f = open(filename, 'r+')
 print(f.read())
+#f.close()
+
+f = open(filename, 'w')
+print(f.write('192.168.1.1\n192.168.1.2'))
 f.close()
